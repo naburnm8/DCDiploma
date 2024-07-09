c62966fec66b0e91959fa5e9443909b45623f1f3
naburnm8 <alint@bk.ru>
2024-07-09 19:25:56 +0300

    added admin panel entries

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from products.models import Product, Category, Shop
+
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Shop)
