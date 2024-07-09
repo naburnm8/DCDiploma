f215c2358e42d39a6b6383f11e5fb8f3a392b66a
naburnm8 <alint@bk.ru>
2024-07-09 19:09:14 +0300

    added cart model

---
+++ cart/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Cart(models.Model):
+    STATUS_CHOICES = [(0, "Создаётся"), (1, "Оплачен"), (2, "Доставлен")]
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default=0)
+    date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.date
+
+
