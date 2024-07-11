abf4c3d30087b594a41c9edfa213e27d0ff43075
naburnm8 <alint@bk.ru>
2024-07-11 12:51:04 +0300

    added order number field in cart model

--- cart/models.py
+++ cart/models.py
@@ -11,6 +11,7 @@ class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
+    orderNumber = models.IntegerField()
 
     def __str__(self):
         return self.date
