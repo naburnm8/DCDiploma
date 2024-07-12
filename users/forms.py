5e0eb7988960d42dd666b848d4d795a59258d0fd
Mikhail Tsapkov <matsapkov@gmail.com>
2024-07-12 21:57:31 +0300

    Промужеточный(или нет...)вариант страниц логина и авторизации

--- users/forms.py
+++ users/forms.py
@@ -1,15 +1,13 @@
 from django import forms
-from django.contrib.auth.forms import (AuthenticationForm, UserChangeForm,
-                                       UserCreationForm)
-
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from users.models import User
-
 
 class UserLoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={
         'class': 'form-control py-4', 'placeholder': 'Введите имя пользователя'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={
         'class': 'form-control py-4', 'placeholder': 'Введите пароль'}))
+
     class Meta:
         model = User
         fields = ('username', 'password')
@@ -20,9 +18,9 @@ class UserRegistrationForm(UserCreationForm):
     last_name = forms.CharField(widget=forms.TextInput(attrs={
         'class': 'form-control py-4', 'placeholder': 'Введите фамилию'}))
     username = forms.CharField(widget=forms.TextInput(attrs={
-        'class': 'form-control py-4', 'placeholder': 'Введите имя пользователя', 'unique': True}))
+        'class': 'form-control py-4', 'placeholder': 'Введите имя пользователя'}))
     email = forms.CharField(widget=forms.EmailInput(attrs={
-        'class': 'form-control py-4', 'placeholder': 'Введите почту', 'unique': True}))
+        'class': 'form-control py-4', 'placeholder': 'Введите почту'}))
     user_address = forms.CharField(widget=forms.TextInput(attrs={
         'class': 'form-control py-4', 'placeholder': 'Введите адрес'}))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={
