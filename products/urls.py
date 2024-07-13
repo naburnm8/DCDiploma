e996a895d0243df47e2edce6ad58629749124585
6i3ek3ua1 <slyantyaev20@yandex.ru>
2024-07-13 19:12:11 +0300

    Добавление фильтрации продуктов по категориям

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,5 @@ from products import views
 app_name = 'products'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('category/<int:category_id>/', views.category, name='category'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-from products.models import Product
+from products.models import Product, Category
 
 
 def index(request):
     context = {
-        'products': Product.objects.all()
+        'products': Product.objects.all(),
+        'categories': Category.objects.all()
+    }
+    return render(request, 'products/index.html', context)
+
+
+def category(request, category_id=None):
+    products = Product.objects.filter(category=category_id) if category_id else Product.objects.all()
+    context = {
+        'products': products,
+        'categories': Category.objects.all()
     }
     return render(request, 'products/index.html', context)
